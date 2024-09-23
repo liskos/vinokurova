f181cfee999a441ee6158dd5ca6b07c93341ba03
Ученик <liskosnik2011@YANDEX.RU>
2024-09-23 10:40:57 +0300

    19 09 2024

---
+++ варианты/шастин/2/2.py
@@ -0,0 +1,7 @@
+for x in 0,1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                f = y and (x or z) or not(y or z) or w
+                if not f:
+                    print(x, y, z, w, int(f))
