487029e67797ab85dda1edc7fd4cb458156130df
Ученик <liskosnik2011@YANDEX.RU>
2025-02-07 08:58:44 +0300

    15 01 25

--- варианты/шастин/6/26.py
+++ варианты/шастин/6/26.py
@@ -2,15 +2,27 @@ def f(filename):
     file = open(filename)
     n = int(file.readline())
     t = [0] * 24 * 60
+    r = [[] for _ in range(24 * 60)]
     for _ in range(n):
         id, t1, t2 =  map(int, file.readline().split())
         for x in range(t1, t2):
             t[x] += 1
+            r[x].append(id)
     m = max(t)
-    for x in range(24*60):
-        if t[x] == m:
-            print(x)
-    return 0, 0
+    b = []
+    for x in range(1, 24*60-1):
+        if t[x] == m and t[x-1] != m:
+            b.append([x])
+        if t[x]==m and t[x+1] != m:
+            b[-1].append(x)
+    print(b)
+    b = sorted(b, key=lambda x:x[1]-x[0], reverse=True)
+    print(b[0])
+    z = set()
+    for x in range(b[0][0], b[0][1]+1):
+        z |= set(r[x])
+    print(z)
+    return len(b), sum(z)
 
 
 
