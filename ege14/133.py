1acb2d58a60fc4fcb8e57f2af0bd44fb5b616133
Ученик <liskosnik2011@YANDEX.RU>
2025-04-03 14:07:09 +0300

    15 01 25

--- ege14/133.py
+++ ege14/133.py
@@ -1,5 +1,6 @@
 k = []
 for x in range(0, 1000):
-    if  str(oct(x))[-3] == "4" and str(oct(x))[-1] == "2" and len(hex(x)[2:]) == 3:
+    if  str(oct(x))[-3] == "4" and str(oct(x))[-1] == "2" and len(hex(x)[2:]) == 3 and len(oct(x)[2:]) == 3:
         k.append(x)
+        print(x)
 print(len(k))
--- ege14/135.py
+++ ege14/135.py
@@ -1,6 +1,6 @@
 k =0
 for x in range(0, 1000):
-    if len(oct(x)[2:]) == 3 and len(hex(x)[2:]) == 3 and hex(x)[-2] == "a":
+    if len(oct(x)[2:]) == 3 and len(hex(x)[2:]) == 2 and hex(x)[-1] == "a":
         k += 1
         print(x)
 print(k)
