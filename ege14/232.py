5729419ac4237e530650a885b30b752854acb668
Ученик <liskosnik2011@YANDEX.RU>
2025-04-11 09:04:04 +0300

    15 01 25

--- ege14/232.py
+++ ege14/232.py
@@ -1,9 +1,11 @@
-for x in range(1,1000):
-    for y in range(1, 1000):
+a = []
+for x in range(1,100):
+    for y in range(1, 100):
         s = (3+2*4**x)* 4 **x+3+4**y
         ss = []
         while s > 0:
             ss.append(s % 4)
             s = s // 4
-print(max(str(ss)))
+        a.append(sum(ss))
+print(max(a))
     
