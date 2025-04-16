b7cb6edf5f558d18e958b0744c9d868cb885064d
Ученик <liskosnik2011@YANDEX.RU>
2025-04-16 09:00:49 +0300

    15 01 25

--- варианты/горбачев/4/12.py
+++ варианты/горбачев/4/12.py
@@ -1,11 +1,11 @@
 def f(s):
     while "96" in s or "366" in s or "666" in s:
         if "96" in s:
-            s = s.replace("96", "6")
+            s = s.replace("96", "6", 1)
         if "366" in s:
-            s = s.replace("366", "69")
+            s = s.replace("366", "69", 1)
         if "666" in s:
-            s = s.replace("666", "3")
+            s = s.replace("666", "3", 1)
     return s
 
 a = set()
